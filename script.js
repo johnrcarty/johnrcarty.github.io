621b7974867f333c760b8e9a80d40c0522b86e97
John Carty <john.carty@gmail.com>
2016-07-20 18:38:03 -0400

    added js file to manage links in html pages

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("navMenu").innerHTML =
+'<ul class="navbar">' +
+  '<li class="navbar navlink"><a href="index.html">Home</a></li>' +
+  '<li class="navbar navlink"><a href="about.html">About</a></li>' +
+  '<li class="navbar navlink"><a href="projects.html">Projects</a></li>' +
+'<li class="navbar navlink"><a href="contact.html">Contact</a></li>' +
+'</ul>';
