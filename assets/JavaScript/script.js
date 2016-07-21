15e996dae4bd5d3f6d7144bf2d1581e2d216e43c
John Carty <john.carty@gmail.com>
2016-07-20 21:51:17 -0400

    removed js code that handled nav bar. updated css to make site prettier. filled pages with lorum ipsum

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,7 +1,7 @@
 document.getElementById("navMenu").innerHTML =
 '<ul class="navbar">' +
-  '<li class="navbar navlink"><a href="index.html">Home</a></li>' +
-  '<li class="navbar navlink"><a href="about.html">About</a></li>' +
-  '<li class="navbar navlink"><a href="projects.html">Projects</a></li>' +
-'<li class="navbar navlink"><a href="contact.html">Contact</a></li>' +
+  '<li><a href="index.html">Home</a></li>' +
+  '<li><a href="about.html">About</a></li>' +
+  '<li><a href="projects.html">Projects</a></li>' +
+'<li><a href="contact.html">Contact</a></li>' +
 '</ul>';
