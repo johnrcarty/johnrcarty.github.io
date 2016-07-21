3b59cff38ac6aeb74e5700b1354177115be6c253
John Carty <john.carty@gmail.com>
2016-07-20 21:52:37 -0400

    emptied js file

--- assets/JavaScript/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("navMenu").innerHTML =
-'<ul class="navbar">' +
-  '<li><a href="index.html">Home</a></li>' +
-  '<li><a href="about.html">About</a></li>' +
-  '<li><a href="projects.html">Projects</a></li>' +
-'<li><a href="contact.html">Contact</a></li>' +
-'</ul>';
